60931786c4f76bc6db82c80e83ccb9183446a476
Sebastian Barkett <barkes2@rpi.edu>
2021-02-17 17:36:00 -0500

    Add files via upload

---
+++ LAB1.R
@@ -0,0 +1,57 @@
+#read in csv file - 
+GPW3_GRUMP_SummaryInformation_2010.csv
+#or read in excel file directly or by 
+csv convert - 2010EPI_data.xls #(EPI2010_all countries or EPI2010_onlyEPIcountries tabs)
+#also for other datasewts enter these in R command window pannel or cmd line
+data()
+help(data)
+EPI_data <- read.csv("2010EPI_data.csv")
+#Note: replace default data frame name - cannot start with numbers! Munging
+#Note: replace <path> with either a directory path or use setwd("<path>")
+View(EPI_data)
+attach(EPI_data) #set the 'default' object
+fix(EPI_data) #launches a simple data editor - test it
+EPI #prints out values EPI_data$EPI
+tf <- is.na(EPI) #records True value is NA
+E <- EPI[!tf] #filters out NA values, new array
+summary(EPI) #stats
+fivenum(EPI,na.rm=TRUE)
+stem(EPI) #stem and leaf plot
+hist(EPI)
+hist(EPI, seq(30., 95., 1.0), prob=TRUE)
+lines(density(EPI,na.rm=TRUE,bw=1.)) # or try bw="SJ"
+rug(EPI)
+#Use help(<comman>), e.g. >help(stem)
+#See group1/lab1_summary.R
+#Save your plots, name them
+#Save commands you used to generate them
+plot(ecdf(EPI), do.points=FALSE, verticals=TRUE) #cumulative density function
+par(pty="s")
+qqnorm(EPI); qqline(EPI) #Quantile-Quantile?
+qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn") #make a Q-Q plot against the generating dist by: x<-seq(30,95,1)
+qqline(x)
+#Excercise 1: fitting a distribution
+#do the same exploration and fitting for another 2 variables in the EPI_data
+#(i.e. primary variables (DALY, WATER_H, ...)
+#Try fitting other distributions - i.e. as ecdf or qq-
+boxplot(EPI,DALY) #Comparing distributions
+qqplot(EPI,DALY)
+#intercompare: EPI, ENVHEALTH, ECOSYSTEM, DALY, AIR_H, WATER_H, AIR_EWATER_E, BIODIVERSITY **(subject to possible filtering)
+#Note the 2010 and 2016 datasets
+#See archive "alldist.zip" or individual files in /distribution
+#Excel files for dif distributions; expanded in the folder: e.g. lognorm.xls
+help(distributions) #In R
+
+#Excercise 2 Conditional Filtering (below)
+EPILand<-EPI[!Landlock]
+Eland <- EPILand[!is.na(EPILand)]
+hist(ELand)
+hist(ELand, seq(30., 95., 1.0), prob=TRUE)
+#Repeat Excersize 1 look at: No_surface_water, Desert and High_Population_Density
+
+#My Excercise: how to filter on EPI_regions or GEO_subregion?
+#E.g. EPI_South_Asia <- EPI[<what is this>]
+#GPW3_GRUMP - repeat: the reading in, then exploration/summary, plots, histograms, distributions, filtering
+#water-treatment.csv #water_treatment 
+
+#Lab objectives: Get familiar with Distributions, Populations, Fitting, & Creating Data Frames in R
