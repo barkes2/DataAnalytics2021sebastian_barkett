2edfe5058bc376c1719f4e205a6a152db80583ca
Sebastian Barkett <barkes2@rpi.edu>
2021-03-19 09:12:32 -0400

    Add files via upload

--- DataAnalytics_A4_SEBASTIAN_BARKETT.R
+++ DataAnalytics_A4_SEBASTIAN_BARKETT.R
@@ -6,4 +6,11 @@ head(queens)
 #View(queens)
 #How many tax classes live in queens?
 summary(TAX.CLASS.AT.TIME.OF.SALE)
-#only 4 classes represented in queens?
+#only 4 tax classes represented in queens?
+#Lets start by converting rows with zero values for GROSS.SQUARE.FEET to NA
+#and then removing them in an updated data frame
+queens$GROSS.SQUARE.FEET[queens$GROSS.SQUARE.FEET==0]<-NA
+tf<-is.na(queens$GROSS.SQUARE.FEET)
+queensGSFNZ<-queens$GROSS.SQUARE.FEET[!tf]
+queensGSFNZ
+#New data frame created that only has non-zero values for GROSS.SQUARE.FEET
